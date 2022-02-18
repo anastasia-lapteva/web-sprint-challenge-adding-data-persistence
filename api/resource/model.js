a6a7413b728af8765fec37d662c5199d09ec6fd3
Anastasia Lapteva <anastasia_laptev@hotmail.com>
2022-02-17 21:32:40 -0800

    wip resource

--- api/resource/model.js
+++ api/resource/model.js
@@ -8,7 +8,37 @@ async function getAll()
     return resources;
 }
 
+async function get(resource_id)
+{
+    const resource = await db('resources')
+        .select('resource_id', 'resource_name', 'resource_description')
+        .where('resource_id', resource_id);
+
+    return resource;
+}
+
+async function exists(name)
+{
+    const resources = await db('resources')
+        .where('resource_name', name);
+
+    return resources.length > 0;
+}
+
+function add(resource)
+{
+    return db('resources')
+        .insert(resource)
+        .then(([resource_id]) =>
+        {
+            return get(resource_id);
+        });
+}
+
 module.exports =
 {
-    getAll
+    getAll,
+    get,
+    exists,
+    add
 };
--- api/resource/router.js
+++ api/resource/router.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const { checkResourceNameUnique } = require('./middleware.js');
 const Resources = require('./model.js');
 
 const router = express.Router();
@@ -10,6 +10,33 @@ router.get('/', (req, res, next) =>
         .then(resources =>
         {
             res.json(resources);
+        })
+        .catch(next);
+});
+
+router.get('/:id', (req, res, next) =>
+{
+    const { id } = req.params;
+
+    Resources.get(id)
+        .then(resource =>
+        {
+            res.status(200).json(resource);
+        })
+        .catch(error =>
+        {
+            next(error);
+        });
+});
+
+router.post('/', checkResourceNameUnique, (req, res, next) =>
+{
+    const resource = req.body;
+
+    Resources.add(resource)
+        .then(resource =>
+        {
+            res.status(201).json(resource);
         })
         .catch(next);
 });
