5a684e1ac0d0305e52cc4a764c8602adcb4150ce
Anastasia Lapteva <anastasia_laptev@hotmail.com>
2022-02-17 18:45:36 -0800

    set up index.js and server.js

--- api/server.js
+++ api/server.js
@@ -1,1 +1,15 @@
-// build your server here and require it from index.js
+const express = require('express');
+
+const jsonErrorHandler = async (err, req, res, next) =>
+{
+    res.header("Content-Type", 'application/json');
+
+    res.status(err.status).send(JSON.stringify(err));
+};
+
+const server = express();
+
+server.use(express.json());
+server.use(jsonErrorHandler);
+
+module.exports = server;
